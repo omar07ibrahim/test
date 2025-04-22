e91881ef64e5d186115b2b12efafdc3d004c0e6d
Omar Ibrahim <31526072+omar07ibrahim@users.noreply.github.com>
2025-04-23 02:31:58 +0400

    Update admin.py

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 from .models import User, Role
+from django.utils.translation import gettext_lazy as _
 
 @admin.register(Role)
 class RoleAdmin(admin.ModelAdmin):
