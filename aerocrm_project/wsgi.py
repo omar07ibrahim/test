d33d4879d8fef13eb38ea51879a6e6416ac09605
Omar Ibrahim <31526072+omar07ibrahim@users.noreply.github.com>
2025-04-23 02:38:25 +0400

    Update wsgi.py

--- aerocrm_project/wsgi.py
+++ aerocrm_project/wsgi.py
@@ -7,3 +7,10 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aerocrm_project.settings')
 application = get_wsgi_application()
 
 
+
+if os.environ.get("DJANGO_SUPERUSER_AUTO_CREATE", "") == "true":
+    try:
+        from scripts.create_superuser import run
+        run()
+    except Exception as e:
+        print("❗ Ошибка при создании суперпользователя:", e)
