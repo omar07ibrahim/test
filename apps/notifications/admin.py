36fc20f1cdf77173e4262ddda3593212e76663ff
Omar Ibrahim <31526072+omar07ibrahim@users.noreply.github.com>
2025-04-23 02:30:02 +0400

    Update admin.py

--- apps/notifications/admin.py
+++ apps/notifications/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Notification
+from django.utils.translation import gettext_lazy as _
 
 @admin.register(Notification)
 class NotificationAdmin(admin.ModelAdmin):
